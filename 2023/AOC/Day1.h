7fef464c0e7da9a3453997ede82554abc7001864
magnpers <magnus.a.persson@sogeti.com>
2023-12-09 01:19:49 +0100

    Renaming and re-structuring and got stuff to work still

---
+++ 2023/AOC/Day1.h
@@ -0,0 +1,49 @@
+#include <regex>
+#include "DataFetch.h"
+using namespace std;
+
+
+namespace Day1
+{
+    void run()
+    {
+        int calibrationSum = 0;
+
+        string inputData = DataFetch::readFileToString("1");
+        int size = inputData.length();
+        string* numbers = new string[size];
+
+        int numbersRow = 0;
+        for (int i = 0; i < inputData.length(); i++)
+        {
+            char currChar = inputData[i];
+            if (currChar == '\n')
+                numbersRow++;
+            else if (isdigit(currChar))
+                numbers[numbersRow] += currChar;
+        }
+
+        int total = 0;
+
+        for (int i = 0; i < numbersRow; i++)
+        {
+            string numberString = numbers[i];
+            string tempString = "";
+
+            if (numberString.length() < 2)
+            {
+                tempString += numberString[0];
+                tempString += numberString[0];
+            }
+            else
+            {
+                tempString += numberString[0];
+                tempString += numberString[numberString.length() - 1];
+            }
+
+            total += stoi(tempString);
+        }
+
+        std::cout << total;
+    }
+}
---
+++ 2023/AOC/Day2.h
@@ -0,0 +1,14 @@
+#include "DataFetch.h"
+using namespace std;
+
+namespace Day2
+{
+	void run()
+	{
+		int calibrationSum = 0;
+
+		string inputData = DataFetch::readFileToString("2");
+
+		cout << inputData;
+	}
+}
