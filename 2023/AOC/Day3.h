44ab81a886a15796772c00e2d60498795875ec61
Magnus Persson <magnus.persson@external.epiroc.com>
2023-12-15 16:36:55 +0100

    Continued the day 3

--- 2023/AOC/Day3.h
+++ 2023/AOC/Day3.h
@@ -1,25 +1,83 @@
 #include <regex>
 #include <iostream>
+#include <map>
+
 using namespace std;
 
 namespace Day3
 {
+	inline void ControlRows(string* row1, string* row2, int rowLength, int& summary)
+	{
+		for (int i = 0; i <= row1->length(); i++)
+		{
+			char currChar = (*row1)[i];
+
+			if (isdigit(currChar))
+			{
+
+			}
+			else
+			{
+				if (currChar != '.')
+				{
+
+				}
+			}
+		}
+	}
+
     inline void Run(string inputData)
     {
         int calibrationSum = 0;
 
         int size = inputData.length();
-        string* row1 = new string[100];
-        string* row2 = new string[100];
-        string* row3 = new string[100];
+        string* compareRow1 = new string[100];
+        string* compareRow2 = new string[100];
+		
+		int summary;
+        int inputRow = 1;
 
-        int inputRow = 0;
+		int i = 0, rowLength = 0;
 
-        for (int i = 0; i < inputRow; i++)
-        {
+		while (i < inputData.length())
+		{
 
-            //total += stoi(tempString);
-        }
+			char currChar = inputData[i];
+
+			if(inputRow == 1)
+				compareRow1[i] = currChar;
+			else
+				compareRow2[i] = currChar;
+
+			if (currChar == '\n')
+			{
+				if(inputRow == 1)
+					ControlRow(compareRow1);
+				else
+					ControlRow(compareRow2);
+
+				if (inputRow == 1)
+				{
+					ControlRows(compareRow1, compareRow2);
+					inputRow = 2;
+				}
+				else 
+				{
+					compareRow2[i] = currChar;
+					compareRow1 = compareRow2;
+
+					compareRow2 = new string[100];
+				}
+				rowLength = 0;
+			}
+
+			if (i + 1 < inputData.length())
+				i++;
+			rowLength++;
+				
+		}
+			cout << "The summary = " << summary;
+	}
 
         //std::cout << total;
     }
